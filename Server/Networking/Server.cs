249382bd31a233acfba7461e56d044a82cca4cde
un4ckn0wl3z <slickerth@gmail.com>
2019-04-22 22:47:54 +0700

    init

---
+++ Server/Networking/Server.cs
@@ -0,0 +1,41 @@
+﻿using Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Networking
+{
+    class Server : IDisposable
+    {
+        public event EventHandler<bool> ServerStateChanged; // event server
+        public event EventHandler<bool> ClientStateChanged; // evant client 
+        public event EventHandler<IPacket> ClientMessageReceived; //
+
+        // define socket
+        private Socket socket;
+
+
+
+        public bool Listen(int port)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
+            socket = new Socket(endPoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+
+            return false;
+
+
+        }
+
+
+        public void Dispose()
+        {
+            socket.Shutdown(SocketShutdown.Both); // dispose socket
+            socket.Close(); // close socket
+        }
+    }
+}
---
+++ Server/Serialization/PacketSerializer.cs
@@ -0,0 +1,67 @@
+﻿using Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Serialization
+{
+    class PacketSerializer
+    {
+
+        private BinaryFormatter formatter;
+
+        public PacketSerializer() : base()
+        {
+            formatter = new BinaryFormatter();
+        }
+
+        public void Serialize(Stream stream, IPacket packet)
+        {
+            formatter.Serialize(stream, packet);
+        }
+
+        public IPacket Deserialize(Stream stream)
+        {
+            formatter.Binder = new BindChanger();
+            return (IPacket)formatter.Deserialize(stream);
+        }
+    }
+
+
+
+    public class BindChanger : SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            string modifiedTypeName = GetModifiedTypeName(typeName);
+            return Type.GetType(string.Format("{0}, {1}", modifiedTypeName, Assembly.GetEntryAssembly().FullName));
+        }
+
+        private string GetModifiedTypeName(string originalTypeName)
+        {
+            string oldRootNamespace = GetRootNamespace(originalTypeName);
+            string currentRootNamespace = GetRootNamespace();
+            return originalTypeName.Replace(oldRootNamespace, currentRootNamespace);
+        }
+
+        private string GetRootNamespace()
+        {
+            string assemblyName = Assembly.GetEntryAssembly().FullName;
+            return assemblyName.Substring(0, assemblyName.IndexOf(','));
+        }
+
+        private string GetRootNamespace(string originalTypeName)
+        {
+            return originalTypeName.Substring(0, originalTypeName.IndexOf('.'));
+        }
+
+    }
+
+
+    }
